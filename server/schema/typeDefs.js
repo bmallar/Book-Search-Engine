f7adc084b79257e71af0a5da0d63a3c81cca406d
bmallar <benmallar@gmail.com>
2024-05-02 17:08:26 -0500

    back end running with apollo

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+}
+type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+}
+
+input bookInput {
+    BookId: ID
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+}
+
+type Auth {
+    token: ID
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    addUser(username: String, email: String, password: String): Auth
+    login(email: String, password: String): Auth
+    saveBook(bookData: bookInput): User
+    removeBook(bookId: ID): User
+}
+
+`
+
+module.exports = typeDefs;
